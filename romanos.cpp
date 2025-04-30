3f2ee5b37c36a54c54071cf630c80ab06769e00c
Lordarco <fernandonunes506@gmail.com>
2025-04-30 18:23:59 -0300

    Refatoração e implementação da função do numero romano "III".

--- romanos.cpp
+++ romanos.cpp
@@ -7,24 +7,46 @@ int romanos_para_decimal(char const * num_romano){
 
 int contador_I = 0, contador_V = 0, contador_X = 0, contador_L = 0, contador_C = 0, contador_D = 0, contador_M = 0;
 int tamanho = strlen(num_romano);
+int total = 0;
 
-  for(int i=0;i<tamanho;i++){
-    if(num_romano[i] == 'I') contador_I++;
-    if(num_romano[i] == 'V') contador_V += 5;
-    if(num_romano[i] == 'X') contador_X += 10;
-    if(num_romano[i] == 'L') contador_L += 50;
-    if(num_romano[i] == 'C') contador_C += 100;
-    if(num_romano[i] == 'D') contador_D += 500;
-    if(num_romano[i] == 'M') contador_M += 1000;
+
+for(int i = 0; i < tamanho ;i++){
+  if (num_romano[i] == 'I'){
+    contador_I++;
+    total += 1;
   }
+  else if(num_romano[i] == 'V'){
+    contador_V++;
+    total += 5;
+  }
+  else if(num_romano[i] == 'X'){
+    contador_X += 10;
+    total += 10;
+  }
+  else if(num_romano[i] == 'L'){
+     contador_L ++;
+     total += 50;
+  }
+  else if(num_romano[i] == 'C'){
+    contador_C ++;
+    total += 100;
+  }
+  else if(num_romano[i] == 'D'){
+    contador_D ++;
+    total += 500;
+  }
+  else if(num_romano[i] == 'M'){
+    contador_M ++;
+    total += 1000;
+  }
+}
 
-  if(contador_I >= 3 and contador_V > 2){
-    return -1;
-    }
-    else{
-      return contador_I;
-    }
-
+if(contador_I > 3 || contador_V > 1) {
   return -1;
+}
+
+
+return total;
+
 }
  

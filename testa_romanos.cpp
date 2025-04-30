70aaacb86e04a04025b8d9130f071d7321df6703
Lordarco <fernandonunes506@gmail.com>
2025-04-30 18:26:49 -0300

    Teste para "XXX"

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -16,8 +16,8 @@ TEST_CASE("Verificacao números básicos", "[romanos]") {
 }
 TEST_CASE("Verificação números repetidos", "[romanos]"){
     REQUIRE( romanos_para_decimal("III") == 3);
-    REQUIRE(romanos_para_decimal("VV") == -1);
-    
+    REQUIRE( romanos_para_decimal("VV") == -1);
+    REQUIRE( romanos_para_decimal("XXX") == 3);
 }
 
 

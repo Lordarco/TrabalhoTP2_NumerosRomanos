f25d55d8a44734d8851f3005a8223eb059140c97
Lordarco <fernandonunes506@gmail.com>
2025-04-30 18:34:58 -0300

    Sucesso teste

--- romanos.cpp
+++ romanos.cpp
@@ -41,7 +41,7 @@ for(int i = 0; i < tamanho ;i++){
   }
 }
 
-if(contador_I > 3 || contador_V > 1 || contador_X > 3) {
+if(contador_I > 3 || contador_V > 1 || contador_X > 3 || contador_L > 1) {
   return -1;
 }
 

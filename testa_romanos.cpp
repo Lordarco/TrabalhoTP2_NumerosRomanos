1c1b276b311fc2c34bd439145f00bf682ba303c1
Lordarco <fernandonunes506@gmail.com>
2025-04-30 13:33:57 -0300

    Teste 7: Implementação e Falha do teste para numero romano "D"

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -10,6 +10,7 @@ TEST_CASE("Conversão de números básicos", "[romanos]") {
     REQUIRE( romanos_para_decimal("X") == 10);
     REQUIRE( romanos_para_decimal("L") == 50);
     REQUIRE( romanos_para_decimal("C") == 100);
+    REQUIRE( romanos_para_decimal("D") == 500);
 }
 
 

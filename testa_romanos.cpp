f69da6c65f4dd3820318d25c9797b528bc98a9ff
Lordarco <fernandonunes506@gmail.com>
2025-04-30 12:30:41 -0300

    Teste 4: falha retorna número romano(X) invalido.

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -7,7 +7,7 @@
 TEST_CASE("Conversão de números básicos", "[romanos]") {
     REQUIRE( romanos_para_decimal("I") == 1 );
     REQUIRE( romanos_para_decimal("V") == 5 );
-    
+    REQUIRE( romanos_para_decimal("X") == 10);
 }
 
 

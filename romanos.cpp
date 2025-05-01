de6a8ed69ca903d549bd7533aeb78e76ecbbb1f8
Lordarco <fernandonunes506@gmail.com>
2025-04-30 22:09:12 -0300

    Refatoração e teste de "IV" com sucesso.

--- romanos.cpp
+++ romanos.cpp
@@ -3,6 +3,19 @@
 #include "romanos.hpp"
 #include <cstring>
 
+int valor_romano(char c) {
+  switch (c) {
+      case 'I': return 1;
+      case 'V': return 5;
+      case 'X': return 10;
+      case 'L': return 50;
+      case 'C': return 100;
+      case 'D': return 500;
+      case 'M': return 1000;
+      default: return -1;
+  }
+}
+
 int romanos_para_decimal(char const * num_romano){
 
 int contador_I = 0, contador_V = 0, contador_X = 0, contador_L = 0, contador_C = 0, contador_D = 0, contador_M = 0;
@@ -10,14 +23,25 @@ int tamanho = strlen(num_romano);
 int total = 0;
 
 
-for(int i = 0; i < tamanho ;i++){
+for(int i = 0; i < tamanho;i++){
+  int atual = valor_romano(num_romano[i]);
+  int proximo = valor_romano(num_romano[i+1]);
+
   if (num_romano[i] == 'I'){
     contador_I++;
-    total += 1;
-  }
+    if(atual < proximo){
+      total += proximo - atual;
+      i++;
+    } 
+    else total += 1;
+  }  
   else if(num_romano[i] == 'V'){
     contador_V++;
-    total += 5;
+    if(atual < proximo){
+      total += proximo - atual;
+      i++;
+    }
+    else total += 5;
   }
   else if(num_romano[i] == 'X'){
     contador_X ++;
@@ -47,6 +71,6 @@ if(contador_I > 3 || contador_V > 1 || contador_X > 3 || contador_L > 1 || conta
 
 
 return total;
-
 }
+
  

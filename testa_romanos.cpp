445abc57ffa2190fc13f7bafd853c3937a8eebbd
Lordarco <fernandonunes506@gmail.com>
2025-04-30 23:34:52 -0300

    Teste falho

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -28,6 +28,7 @@ TEST_CASE("Verificação números repetidos", "[romanos]"){
 TEST_CASE("Numeros para Subtrair", "[romanos]"){
     REQUIRE( romanos_para_decimal("IV") == 4);
     REQUIRE( romanos_para_decimal("IX") == 9);
+    REQUIRE( romanos_para_decimal("XL") == 40 );
 }
 
 /*
